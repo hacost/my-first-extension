c765dd1b19c90fa7a6e614e7d2bc5d6e8b268b5c
Héctor Acosta <hacost@hotmail.com>
2022-06-23 12:48:15 -0500

    my first commit

---
+++ background.js
@@ -0,0 +1,6 @@
+//background.js
+  let color = '#3aa757';
+  chrome.runtime.onInstalled.addListener( () => {
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+  });
